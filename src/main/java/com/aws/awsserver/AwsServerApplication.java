c217e2cb4b1b199d305081ec68299dbcd068d4a0
DESKTOP-N6DV9P0\WallaceLAM <hoiping.lam@ust.com>
2022-07-15 10:37:10 +0800

    spring ini with https://start.spring.io/

---
+++ src/main/java/com/aws/awsserver/AwsServerApplication.java
@@ -0,0 +1,13 @@
+package com.aws.awsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsServerApplication.class, args);
+	}
+
+}
